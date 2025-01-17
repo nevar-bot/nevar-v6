55117d7fe7e95d1d81c5bad66f6d0fd1bd58b98c
Florian <mrminecrafttv19@gmail.com>
2025-01-17 11:19:01 +0100

    :bug: fix: better error handling for welcome/farewell system (fixes #2)

--- src/events/member/GuildMemberRemove.ts
+++ src/events/member/GuildMemberRemove.ts
@@ -10,7 +10,7 @@ class GuildMemberRemoveEvent extends BaseEvent {
 	}
 
 	public async run(removedGuildMember: GuildMember): Promise<void> {
-		if(!removedGuildMember || !removedGuildMember.id || !removedGuildMember.guild) return;
+		if (!removedGuildMember || !removedGuildMember.id || !removedGuildMember.guild) return;
 		const { guild } = removedGuildMember;
 
 		const guildUtilsInstance: GuildUtils = new GuildUtils(this.client, guild);
@@ -29,36 +29,64 @@ class GuildMemberRemoveEvent extends BaseEvent {
 		await guildUtilsInstance.log(memberLogEmbed, 'member');
 
 		// Goodbye message
-		if(guildData.settings.farewell.enabled){
-			function parseFarewellMessage(message: string): string {
-				return message
-					.replaceAll('?user.displayName', removedGuildMember.displayName)
-					.replaceAll('?user.name', removedGuildMember.user.username)
-					.replaceAll('?user', removedGuildMember.toString())
-					.replaceAll('?server.name', guild.name)
-					.replaceAll('?server.memberCount', guild.memberCount.toString())
-					.replaceAll('?newline', '\n');
+		if (guildData.settings.farewell.enabled) {
+			const guildMessage: string = guildData.settings.farewell.message;
+
+			if (!guildMessage) {
+				const embedText: string =
+					'### ' + this.emote('error') + ' Verabschiedungsnachricht nicht gefunden\n\n' +
+					'-# ' + this.emote('arrow_right') + ' **Fehler**: Es wurde keine Verabschiedungsnachricht gesetzt. Bitte setze die Verabschiedungsnachricht mit `/goodbye nachricht`.';
+
+				const serverLogEmbed: EmbedBuilder = this.clientUtils.createEmbed(embedText, null, 'error').setThumbnail(this.client.user.displayAvatarURL());
+				await guildUtilsInstance.log(serverLogEmbed, 'guild');
+				return;
 			}
 
-			const farewellMessage: string = parseFarewellMessage(guildData.settings.farewell.message);
+			const farewellMessage: string = this.parseFarewellMessage(guildMessage, removedGuildMember, guild);
 
 			const channel: any = guild.channels.cache.get(guildData.settings.farewell.channelId);
-			if(!channel) return;
+			if (!channel) {
+				const embedText: string =
+					'### ' + this.emote('error') + ' Verabschiedungskanal nicht gefunden\n\n' +
+					'-# ' + this.emote('arrow_right') + ' **Fehler**:  Der Verabschiedungskanal wurde nicht gefunden. Bitte setze den Verabschiedungskanal mit `/goodbye kanal`.';
 
-			if(!guild.members.me.permissionsIn(channel.id).has(PermissionsBitField.Flags.SendMessages)) return;
-			if(!guildData.settings.farewell.color) return;
+				const serverLogEmbed: EmbedBuilder = this.clientUtils.createEmbed(embedText, null, 'error').setThumbnail(this.client.user.displayAvatarURL());
+				await guildUtilsInstance.log(serverLogEmbed, 'guild');
+				return;
+			}
+
+			if (!guild.members.me.permissionsIn(channel.id).has(PermissionsBitField.Flags.SendMessages)) return;
+			if (!guildData.settings.farewell.color) {
+				const embedText: string =
+					'### ' + this.emote('error') + ' Verabschiedungsfarbe nicht gefunden\n\n' +
+					'-# ' + this.emote('arrow_right') + ' **Fehler**: Die Verabschiedungsfarbe wurde nicht gefunden. Bitte setze die Verabschiedungsfarbe mit `/goodbye farbe`.';
+
+				const serverLogEmbed: EmbedBuilder = this.clientUtils.createEmbed(embedText, null, 'error').setThumbnail(this.client.user.displayAvatarURL());
+				await guildUtilsInstance.log(serverLogEmbed, 'guild');
+				return;
+			}
 
 			const farewellEmbed: EmbedBuilder = new EmbedBuilder()
 				.setDescription(farewellMessage)
 				.setColor(guildData.settings.farewell.color as ColorResolvable)
 				.setFooter({ text: config.get('embeds.footer_text') });
 
-			if(guildData.settings.farewell.avatar){
+			if (guildData.settings.farewell.avatar) {
 				farewellEmbed.setThumbnail(removedGuildMember.displayAvatarURL());
 			}
 
 			await channel.send({ embeds: [farewellEmbed] });
 		}
+	}
+
+	private parseFarewellMessage(message: string, removedGuildMember: GuildMember, guild: any): string {
+		return message
+			.replaceAll('?user.displayName', removedGuildMember.displayName)
+			.replaceAll('?user.name', removedGuildMember.user.username)
+			.replaceAll('?user', removedGuildMember.toString())
+			.replaceAll('?server.name', guild.name)
+			.replaceAll('?server.memberCount', guild.memberCount.toString())
+			.replaceAll('?newline', '\n');
 	}
 }
 
